dfb546304494ed4826e50d1ada6e07d29d0bcd2a
Davi Felinto <davihsfelinto@hotmail.com>
2021-09-27 19:35:18 -0300

    commit

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,11 +1,15 @@
-import java.awt.event.KeyListener;
+// import java.awt.event.KeyListener;
 
-public class Jogo implements KeyListener {
+public class Jogo /*implements KeyListener*/ {
     public static void main(String[] args) {
         System.out.println("teste\u001B[30m");
     }
 
+    public void tela (){
+        /**  **/
+    }
 
+    /**
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -13,23 +17,22 @@ public class Jogo implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        /*
         if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A){
             movimento.left = true;
         } else if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D){
             movimento.right = true;
         }
-        */
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        /*
         if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A){
             player.left = false;
         } else if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D){
             player.right = false;
         }
-        */
+
     }
+**/
 }
