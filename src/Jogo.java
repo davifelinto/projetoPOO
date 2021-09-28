814411fad902bdeb44fbbdeb49734d9863765450
Davi Felinto <davihsfelinto@hotmail.com>
2021-09-28 09:55:22 -0300

    commit

--- src/Jogo.java
+++ src/Jogo.java
@@ -3,6 +3,15 @@
 public class Jogo /*implements KeyListener*/ {
     public static void main(String[] args) {
         System.out.println("teste\u001B[30m");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        /*imprimir na tela o tabuleiro*/
+        /*pegar o imput do player da vez*/
+        /*checar se o imput é uma jogada válida*/
+        /*atualizar o tabuleiro*/
+        /*checar regras (check e chekmate)*/
+        /*finalizar o jogo caso checkmate*/
+        /*caso continue o jogo, pegar proximo imput e repetir o processo*/
     }
 
 
