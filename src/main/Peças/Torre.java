d2ca506bb421ceeaedcb1da85524991067c216b8
ItaloVilasBoas <83239045+ItaloVilasBoas@users.noreply.github.com>
2021-10-13 09:08:01 -0300

    Update Torre.java

--- src/main/Peças/Torre.java
+++ src/main/Peças/Torre.java
@@ -10,10 +10,6 @@ public class Torre extends Peca{
 
     public Torre(boolean cor, int lin, int col) {
         super(cor, lin, col);
-        //if(cor)
-        //    Tabuleiro.getCasa(lin, col).setPeca('♖');
-        //else
-        //    Tabuleiro.getCasa(lin, col).setPeca('♜');
     }
 
     @Override
